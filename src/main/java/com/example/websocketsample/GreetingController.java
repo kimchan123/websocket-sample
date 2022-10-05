54c731b23626ae0d556255b3934a1be07d7de538
awesomeo184 <awesomeo184@gmail.com>
2022-10-05 15:55:49 +0900

    예제 작성

---
+++ src/main/java/com/example/websocketsample/GreetingController.java
@@ -0,0 +1,26 @@
+package com.example.websocketsample;
+
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+    private final SimpMessagingTemplate template;
+
+    public GreetingController(SimpMessagingTemplate template) {
+        this.template = template;
+    }
+
+    @MessageMapping("/hello/{id}")
+    public void greeting(@DestinationVariable Long id, HelloMessage message) throws Exception {
+//        Thread.sleep(1000); // simulated delay
+        System.out.println("id = " + id);
+        this.template.convertAndSend("/topic/greeting/" + id,
+                new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!"));
+    }
+
+}
---
+++ src/main/resources/static/app.js
@@ -0,0 +1,56 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+    let socket = new SockJS('/gs-guide-websocket');
+    console.log("===========job=========");
+    console.log($("#job").val());
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/greetings/' + $("#job").val(), function (greeting) {
+            console.log("=========data arrived==========")
+            console.log(greeting);
+            showGreeting(JSON.parse(greeting.body).content);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+    console.log($("#job").val());
+    stompClient.send("/app/hello/" + $("#job").val(), {}, JSON.stringify({'name': $("#name").val()}));
+}
+
+function showGreeting(message) {
+    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendName(); });
+});
+
